a68ac890637e71d5183be8938378861a8ec77bfa
Bitrise CI <noreply@bitrise.io>
2026-02-09 09:32:35 +0000

    Update SPM package for SDK version 10.1.4.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "PradoSDK",
     platforms: [
-        .iOS(.v12) // or your minimum supported version
+        .iOS(.v12)
     ],
     products: [
         .library(
@@ -12,10 +12,13 @@ let package = Package(
             targets: ["PradoSDK"]
         )
     ],
+    
     targets: [
+        
         .binaryTarget(
             name: "PradoSDK",
-            path: "XCFramework/PradoSDK.xcframework"
+            url: "https://github.com/Prado-SDK/prado-ios-frameworks/raw/refs/heads/main/PradoSDK/10.1.4/PradoSDK.zip",
+            checksum: "5d70a6c3210397d662d819f45ab334d6da6eb49b7c8cb94b4627cc07bb660d35"
         )
     ]
 )
